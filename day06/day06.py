8e8bb4d587e6135c1b5cbcf1dde2200ae8756834
Sébastien Delneste <seb@bigup.be>
2021-12-06 10:02:52 +0100

    Day 06 with a timer.

--- day06/day06.py
+++ day06/day06.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-from collections import Counter
+import time
+
 
 def part1(data):
     fishes = [int(x) for x in data[0].split(',')]
@@ -21,6 +22,11 @@ def part1(data):
 
 
 def part2(data):
+    """The list gets too large, I implement a counter of timers.
+    1st element of the list is the number of items set to 0, etc...
+    Every day, list[0] is added at list[6] (reset the timers)
+    and also added at list[8] for the new fishes.
+    """
     fishes = sorted(data[0].replace(',', ''))
     day = 1
     timers =[0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -35,8 +41,12 @@ def part2(data):
 
 def main():
     data = [line.strip() for line in open('input.txt', 'r').readlines()]
-    print('Part 1: %s' % part1(data))
-    print('Part 2: %s' % part2(data))
+    start = time.perf_counter()
+    print('Part 1: %s' % part1(data), end=' ')
+    print ('[%05f secs]' % (time.perf_counter() - start))
+    start = time.perf_counter()
+    print('Part 2: %s' % part2(data), end=' ')
+    print ('[%05f secs]' % (time.perf_counter() - start))
 
 
 if __name__ == "__main__":
